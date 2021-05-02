314512f8b6ac56dc3efa71aa2d60d8221862c2df
Fenti08 <58581075+Fenti08@users.noreply.github.com>
2021-05-02 21:45:08 +0700

    Minggu 12
    Minggu 12

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDrCFSAlMMrgTqWgQ-MCKm7Sa6wnBkJJ6I",
+    authDomain: "fir-crud-d2c4c.firebaseapp.com",
+    projectId: "fir-crud-d2c4c",
+    storageBucket: "fir-crud-d2c4c.appspot.com",
+    messagingSenderId: "836774037736",
+    appId: "1:836774037736:web:cdbed654690fc9428b215d",
+    measurementId: "G-0TY614FMD9"
+  };
+
+export default firebaseConfig;
